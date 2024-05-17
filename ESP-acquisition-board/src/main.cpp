20193d09c6484429f4bb5ee48f197e30c58008ae
RaresCon <rares.constantin2002@gmail.com>
2024-05-17 21:55:21 +0300

    project: comm: Added comm protocol SERDES stub
    Implemented SERDES functions for the structures
    the ESP NOW own communication protocol will rely
    on to transmit and receive data.
    
    Signed-off-by: RaresCon <rares.constantin2002@gmail.com>

--- ESP-acquisition-board/src/main.cpp
+++ ESP-acquisition-board/src/main.cpp
@@ -7,6 +7,7 @@
 #include <Adafruit_BMP280.h>
 
 Adafruit_BMP280 bmp1, bmp2;
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 void setup() {
     Serial.begin(115200);
@@ -16,6 +17,7 @@ void setup() {
     analogReadResolution(6);
     pinMode(35, INPUT);
     pinMode(34, INPUT);
+    esp_now_peer_info_t peerInfo;
 
     if(!bmp1.begin(0x76)) {
         Serial.println(bmp1.sensorID());
@@ -23,6 +25,23 @@ void setup() {
 
     if(!bmp2.begin(0x77)) {
         Serial.println(bmp1.sensorID());
+    }
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
     }
 }
 
---
+++ README.md
@@ -0,0 +1,14 @@
+## PM Project - Car infotainment system
+### By Rares Constantin
+
+---
+### Description
+
+---
+### Hardware and schematics
+
+### Acquisition board
+![ESP32 - Acquisition board](./ESP-acquisition-board/PM-Acquisition-Board.png)
+
+### Action board
+![ESP32 - Action board](./ESP-action-board/PM-Action-Board.png)
